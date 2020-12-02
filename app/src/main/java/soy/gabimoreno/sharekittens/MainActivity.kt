ed990f66d208f6422a74fba3e4faf8a89c28f523
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-12-02 17:43:27 +0100

    1st commit

---
+++ app/src/main/java/soy/gabimoreno/sharekittens/MainActivity.kt
@@ -0,0 +1,11 @@
+package soy.gabimoreno.sharekittens
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
