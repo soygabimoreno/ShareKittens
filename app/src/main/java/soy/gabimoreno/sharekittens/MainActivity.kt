78f8a1b4ba55f44377b15dbea5e300333ee15f2e
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-12-03 15:29:15 +0100

    Checked that the Giphy library works

--- app/src/main/java/soy/gabimoreno/sharekittens/MainActivity.kt
+++ app/src/main/java/soy/gabimoreno/sharekittens/MainActivity.kt
@@ -2,10 +2,21 @@ package soy.gabimoreno.sharekittens
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.giphy.sdk.ui.Giphy
+import com.giphy.sdk.ui.views.GiphyDialogFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Giphy.configure(
+            this,
+            "Oc0z87qUstm00aqNFHAX6j6riWfdWrQI"
+        )
+        GiphyDialogFragment.newInstance().show(
+            supportFragmentManager,
+            "giphy_dialog"
+        )
     }
 }
