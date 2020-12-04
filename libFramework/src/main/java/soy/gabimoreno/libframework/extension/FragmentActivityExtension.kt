dc89ebb586597c757bf86337e11a2270f78c1a0c
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-12-04 09:54:36 +0100

    Added some extensions

---
+++ libFramework/src/main/java/soy/gabimoreno/libframework/extension/FragmentActivityExtension.kt
@@ -0,0 +1,29 @@
+package soy.gabimoreno.libframework.extension
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+
+fun FragmentActivity.navigateTo(
+    containerResId: Int,
+    fragment: Fragment
+) {
+    supportFragmentManager.beginTransaction()
+        .replace(
+            containerResId,
+            fragment
+        )
+        .commit()
+}
+
+fun FragmentActivity.navigateAddingToBackStackTo(
+    containerResId: Int,
+    fragment: Fragment
+) {
+    supportFragmentManager.beginTransaction()
+        .replace(
+            containerResId,
+            fragment
+        )
+        .addToBackStack(fragment::class.java.simpleName)
+        .commit()
+}
---
+++ libFramework/src/main/java/soy/gabimoreno/libframework/extension/ViewExtension.kt
@@ -0,0 +1,11 @@
+package soy.gabimoreno.libframework.extension
+
+import android.view.View
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.gone() {
+    visibility = View.GONE
+}
